913dc4be93378742183d75d42e2fb77197e1acd9
Sathashini <sathashini4623@gmail.com>
2025-06-10 12:39:21 +0530

    add file

---
+++ src/com/finding/Temp.java
@@ -0,0 +1,4 @@
+package com.finding;
+
+public class Temp {
+}
--- src/com/highest/Highest.java
+++ src/com/highest/Highest.java
@@ -3,28 +3,10 @@ package com.highest;
 import java.util.Scanner;
 
 public class Highest {
-    public void highesttemp(int sensor1,double a,double b,double c,double d,double f) {
-
-        if(a > 28 || a<18){
-            System.out.println("TEMP1 OUT OF RANGE");
+    public void highesttemp(double temp) {
+        if(temp<18 || temp>28){
+            System.out.println(temp+"OUT OF RANGE");
         }
-        if (b>28 || b < 18 ) {
-            System.out.println("TEMP2 OUT OF RANGE");
-
-        }
-        if (c>28 || c<18) {
-            System.out.println("TEMP3 OUT OF RANGE");
-
-        }
-        if (d>28 || d<18) {
-            System.out.println("TEMP4 OUT OF RANGE");
-
-        }
-        if (f>28 || f<18) {
-            System.out.println("TEMP5 OUT OF RANGE");
-
-        }
-
     }
 
     public static void main(String[] args) {
@@ -36,7 +18,11 @@ public class Highest {
         double c=sc.nextDouble();
         double d=sc.nextDouble();
         double f=sc.nextDouble();
-        highest.highesttemp(sensor1,a,b,c,d,f);
+        highest.highesttemp(a);
+        highest.highesttemp(b);
+        highest.highesttemp(c);
+        highest.highesttemp(d);
+        highest.highesttemp(f);
 
     }
 }
