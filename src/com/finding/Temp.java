aa6bf418c618e63efcef817f0cb03988eefe3cf9
Sathashini <sathashini4623@gmail.com>
2025-06-10 12:57:16 +0530

    add file

--- src/com/finding/Temp.java
+++ src/com/finding/Temp.java
@@ -1,4 +1,28 @@
 package com.finding;
 
+import java.util.Scanner;
+
 public class Temp {
+    public void temp(float a){
+        if(a>=18 && a<=20){
+            System.out.println("COOLEST");
+        }
+        if(a>20 && a<=24){
+            System.out.println("COOLER");
+        }
+        if(a>24 && a<=28){
+            System.out.println("COOL");
+        }
+        if(a<18 || a>28){
+            System.out.println("OUT OF RANGE");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        Temp tempe=new Temp();
+        tempe.temp(a);
+    }
 }
