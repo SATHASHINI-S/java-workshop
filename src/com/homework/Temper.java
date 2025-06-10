2c4cce03383f075aa95792c923cfa0419522b080
Sathashini <sathashini4623@gmail.com>
2025-06-10 22:49:09 +0530

    add file

---
+++ src/com/homework/Temper.java
@@ -0,0 +1,21 @@
+package com.homework;
+
+import java.util.Scanner;
+
+public class Temper {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i < n; i++)
+        {
+            arr[i]=sc.nextInt();
+
+        }
+        System.out.println("The Temperatures are");
+        for(int i=0;i < n; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
