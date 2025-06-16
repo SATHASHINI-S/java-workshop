7602b7cf641205575a294c6fa872d7e844e31e9c
Sathashini <sathashini4623@gmail.com>
2025-06-16 10:36:43 +0530

    add file

--- src/com/string/Repeated.java
+++ src/com/string/Repeated.java
@@ -1,22 +1,13 @@
 //package com.string;
 //
-//import java.util.HashMap;
-//import java.util.Map;
 //import java.util.Scanner;
 //
-//public class Repeated {
+//public class Repeated{
 //    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
-//        String repeat = sc.nextLine();
-//        char[] coun = repeat.toCharArray();
-//        Map<Integer,Character> counting = new HashMap<>();
-//        for(int i = 0; i<repeat.length(); i++){
-//            char word = coun[i];
-//            counting.put(word,counting.getOrDefault(word,0)+1);
-//
-//        }
-//
-//
+//        String sentence = sc.nextLine();
+//        String[][] arr = new String[26][2];
 //
 //    }
+//
 //}
