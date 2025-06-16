949977787c296b49df3541c0cdf987d3785be67f
Sathashini <sathashini4623@gmail.com>
2025-06-16 16:56:37 +0530

    add file

--- src/com/string/LocalTimeZone.java
+++ src/com/string/LocalTimeZone.java
@@ -1,4 +1,16 @@
 package com.string;
 
+import java.time.LocalTime;
+import java.util.Scanner;
+
 public class LocalTimeZone {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LocalTime timeNow = LocalTime.of(4,46);
+        int minutes = LocalTime.now().getMinute();
+        int hours = LocalTime.now().getHour();
+        System.out.println((minutes- timeNow.getMinute())+" minutes "+(hours - timeNow.getHour()
+        ));
+
+    }
 }
