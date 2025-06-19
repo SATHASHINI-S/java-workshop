e798bfda41fd047ddda8396bf5786e411630315f
Sathashini <sathashini4623@gmail.com>
2025-06-19 23:45:45 +0530

    add files

--- src/main/java/com/collections/MarksValidation.java
+++ src/main/java/com/collections/MarksValidation.java
@@ -1,4 +1,22 @@
 package com.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class MarksValidation {
+    public static void main(String[] args) {
+        List<Integer> marks = new ArrayList<>();
+        marks.add(58);
+        marks.add(45);
+        marks.add(96);
+        marks.add(85);
+        marks.add(35);
+        Collections.sort(marks);
+        System.out.println(marks);
+        Collections.sort(marks,Comparator.reverseOrder());
+        System.out.println(marks);
+        System.out.println(Collections.max(marks));
+    }
 }
