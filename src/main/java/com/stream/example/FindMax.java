3bee0b924da313c4fc211ea5a27f8da54e7e1e0f
Sathashini <sathashini4623@gmail.com>
2025-08-12 19:36:57 +0530

    String chnage

--- src/main/java/com/stream/example/FindMax.java
+++ src/main/java/com/stream/example/FindMax.java
@@ -2,9 +2,6 @@ package com.stream.example;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FindMax {
     public static void main(String[] args) {
@@ -14,11 +11,12 @@ public class FindMax {
         System.out.println(numberMin);
         System.out.println(numberStream);
 
-//        Stream<Integer> numberStream = Arrays.stream(numbers);
-//        List<Integer> sorted = numberStream.sorted().collect(Collectors.toList());
-//        System.out.println(sorted.get(0));
-//        System.out.println(sorted.getLast());
-//        System.out.println(sorted);
+    //    Stream<Integer> numberStream = Arrays.stream(numbers);
+    //    List<Integer> sorted = numberStream.sorted().collect(Collectors.toList());
+    //    System.out.println(sorted.get(0));
+    //    System.out.println(sorted.getLast());
+    //    System.out.println(sorted);
+    //    System.out.println("abc");
 
 
     }
---
+++ src/main/java/com/string/Basic.java
@@ -0,0 +1,14 @@
+package com.string;
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Basic String");
+
+        String a = "Sathashini";
+        System.out.println(a);
+    }
+
+
+
+    
+}
