ffdefb725ee04372a7c5a0c8f2f07a201a3d75fd
Sathashini <sathashini4623@gmail.com>
2025-08-13 19:54:33 +0530

    ..

--- src/main/java/com/string/Basic.java
+++ src/main/java/com/string/Basic.java
@@ -4,11 +4,6 @@ public class Basic {
     public static void main(String[] args) {
         System.out.println("Basic String");
 
-        String a = "Sathashini";
-        System.out.println(a);
     }
 
-
-
-    
 }
