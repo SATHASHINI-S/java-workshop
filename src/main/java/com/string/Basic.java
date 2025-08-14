9bcc38abdc286f6c989109834b63b4f20827365b
Sathashini <sathashini4623@gmail.com>
2025-08-14 18:23:16 +0530

    ..

--- src/main/java/com/string/Basic.java
+++ src/main/java/com/string/Basic.java
@@ -3,6 +3,7 @@ package com.string;
 public class Basic {
     public static void main(String[] args) {
         System.out.println("Basic String");
+        System.out.println("HelloWorld");
 
     }
 
